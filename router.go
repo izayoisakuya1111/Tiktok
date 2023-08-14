423fc1d34eb9b56bfcbaebdb74e169e47d806d77
izayoisakuya1111 <142203329+izayoisakuya1111@users.noreply.github.com>
2023-08-14 17:17:49 +0800

    Update router.go

--- router.go
+++ router.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-  "github.com/izayoisakuya1111/douyin/controller"
+  	"github.com/izayoisakuya1111/douyin/controller"
 )
 
 func initRouter(r *gin.Engine) {
