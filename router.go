f147fe14a40498ebd988e2dc7623a1671b28f35b
izayoisakuya1111 <142203329+izayoisakuya1111@users.noreply.github.com>
2023-08-14 17:37:38 +0800

    Update router.go

--- router.go
+++ router.go
@@ -6,12 +6,9 @@ import (
 )
 
 func initRouter(r *gin.Engine) {
-	// public directory is used to serve static resources
 	r.Static("/static", "./public")
 
 	apiRouter := r.Group("/douyin")
-
-	// basic apis
 
 	apiRouter.POST("/user/register/", controller.Register)
 	apiRouter.POST("/user/login/", controller.Login)
